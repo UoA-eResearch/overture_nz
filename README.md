f17ad12f346fd46e73b6ecc9ba448c7491a311fc
Nick Young <nick.young@auckland.ac.nz>
2023-08-25 15:54:49 +1200

    init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # overture_nz
 Python script to extract all Overture POIs, deck.gl to plot it
+
+From https://overturemaps.org/  
+Places are licensed under CDLA Permissive v 2.0, see https://cdla.dev/permissive-2-0/
+
+### Downloading
+
+Run:
+
+```docker run -it -v `pwd`:/opt/workspace apache/sedona:latest python3 download.py```
+
+to download to POIs for NZ, and export to CSV format in the current working directory
---
+++ download.py
@@ -0,0 +1,40 @@
+# Run with docker run -it -v `pwd`:/opt/workspace apache/sedona:latest python3 download.py
+
+import time
+from sedona.spark import *
+config = SedonaContext.builder().config("fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider").getOrCreate()
+sedona = SedonaContext.create(config)
+
+df = sedona.read.format("parquet").load("s3a://overturemaps-us-west-2/release/2023-07-26-alpha.0/theme=places/type=place")
+nz = df.filter("ST_Contains(ST_GeomFromWKT('Polygon((160 -30.5,187 -30.5,187 -49.5,160 -49.5, 160 -30.5))'), ST_GeomFromWKB(geometry)) = true")
+
+def geomTypeConverter(df, df_name):
+    new_columns = []
+    # Find the geometry column and convert it to a Sedona geometry type column
+    for col_name in df.schema.names:
+        if col_name == "geometry":
+            new_columns.append("ST_GeomFromWKB(geometry) AS geometry")
+        else:
+            new_columns.append(col_name)
+    cols =','.join(new_columns)
+    df.createOrReplaceTempView(df_name)
+    df = sedona.sql("SELECT " + cols + " FROM "+df_name)
+    df.createOrReplaceTempView(df_name)
+    return df
+
+tbl_name = "df_place"
+nz = geomTypeConverter(nz, tbl_name)
+
+print("Downloading...")
+s = time.time()
+nz = nz.toPandas()
+print(f"Took {round(time.time() - s)}s") # Takes about 121s
+
+nz["lng"] = nz.geometry.apply(lambda p: p.x)
+nz["lat"] = nz.geometry.apply(lambda p: p.y)
+
+nz["name"] = nz.names.apply(lambda d: d["common"][0]["value"])
+nz["main_category"] = nz.categories.apply(lambda c: c.main)
+nz["alternate_categories"] = nz.categories.apply(lambda c: c.alternate)
+
+nz[["name", "main_category", "alternate_categories", "lat", "lng"]].to_csv("overture_nz.csv", index=False)
