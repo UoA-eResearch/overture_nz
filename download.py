9e0f0864e6aa480bc2f734b0414675f87f047ed0
Nick Young <nick.young@auckland.ac.nz>
2023-09-22 11:07:18 +1200

    Switzerland POIs

--- download.py
+++ download.py
@@ -6,7 +6,10 @@ config = SedonaContext.builder().config("fs.s3a.aws.credentials.provider", "org.
 sedona = SedonaContext.create(config)
 
 df = sedona.read.format("parquet").load("s3a://overturemaps-us-west-2/release/2023-07-26-alpha.0/theme=places/type=place")
-nz = df.filter("ST_Contains(ST_GeomFromWKT('Polygon((160 -30.5,187 -30.5,187 -49.5,160 -49.5, 160 -30.5))'), ST_GeomFromWKB(geometry)) = true")
+# NZ
+#df = df.filter("ST_Contains(ST_GeomFromWKT('Polygon((160 -30.5,187 -30.5,187 -49.5,160 -49.5, 160 -30.5))'), ST_GeomFromWKB(geometry)) = true")
+# Switzerland
+df = df.filter("ST_Contains(ST_GeomFromWKT('POLYGON((5.9559 47.8084, 10.4921 47.8084, 10.4921 45.818, 5.9559 45.818, 5.9559 47.8084))'), ST_GeomFromWKB(geometry)) = true")
 
 def geomTypeConverter(df, df_name):
     new_columns = []
@@ -23,18 +26,18 @@ def geomTypeConverter(df, df_name):
     return df
 
 tbl_name = "df_place"
-nz = geomTypeConverter(nz, tbl_name)
+df = geomTypeConverter(df, tbl_name)
 
 print("Downloading...")
 s = time.time()
-nz = nz.toPandas()
+df = df.toPandas()
 print(f"Took {round(time.time() - s)}s") # Takes about 121s
 
-nz["lng"] = nz.geometry.apply(lambda p: p.x)
-nz["lat"] = nz.geometry.apply(lambda p: p.y)
+df["lng"] = df.geometry.apply(lambda p: p.x)
+df["lat"] = df.geometry.apply(lambda p: p.y)
 
-nz["name"] = nz.names.apply(lambda d: d["common"][0]["value"])
-nz["main_category"] = nz.categories.apply(lambda c: c.main)
-nz["alternate_categories"] = nz.categories.apply(lambda c: c.alternate)
+df["name"] = df.names.apply(lambda d: d["common"][0]["value"])
+df["main_category"] = df.categories.apply(lambda c: c.main)
+df["alternate_categories"] = df.categories.apply(lambda c: c.alternate)
 
-nz[["name", "main_category", "alternate_categories", "lat", "lng"]].to_csv("overture_nz.csv", index=False)
+df[["name", "main_category", "alternate_categories", "lat", "lng"]].to_csv("overture_CH.csv", index=False)
