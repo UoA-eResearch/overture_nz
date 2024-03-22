7dcc105ba58e009235f4c5d7c3bad26f0eda698c
Nick Young <nick.young@auckland.ac.nz>
2024-03-22 16:42:05 +1300

    2024-03-12-alpha.0 update

--- download.py
+++ download.py
@@ -1,15 +1,15 @@
 # Run with docker run --rm -it -v `pwd`:/opt/workspace apache/sedona:latest python3 download.py
 
 import time
+import pandas as pd
 from sedona.spark import *
 config = SedonaContext.builder().config("fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider").getOrCreate()
 sedona = SedonaContext.create(config)
 
-df = sedona.read.format("parquet").load("s3a://overturemaps-us-west-2/release/2023-07-26-alpha.0/theme=places/type=place")
+df = sedona.read.format("geoparquet").load("s3a://overturemaps-us-west-2/release/2024-03-12-alpha.0/theme=places/type=place")
+
 # NZ
-#df = df.filter("ST_Contains(ST_GeomFromWKT('Polygon((160 -30.5,187 -30.5,187 -49.5,160 -49.5, 160 -30.5))'), ST_GeomFromWKB(geometry)) = true")
-# Switzerland
-df = df.filter("ST_Contains(ST_GeomFromWKT('POLYGON((5.9559 47.8084, 10.4921 47.8084, 10.4921 45.818, 5.9559 45.818, 5.9559 47.8084))'), ST_GeomFromWKB(geometry)) = true")
+df = df.filter("ST_Contains(ST_GeomFromWKT('Polygon((160 -30.5,187 -30.5,187 -49.5,160 -49.5, 160 -30.5))'), ST_GeomFromWKB(geometry)) = true")
 
 def geomTypeConverter(df, df_name):
     new_columns = []
@@ -27,17 +27,21 @@ def geomTypeConverter(df, df_name):
 
 tbl_name = "df_place"
 df = geomTypeConverter(df, tbl_name)
+print(df)
 
 print("Downloading...")
 s = time.time()
 df = df.toPandas()
 print(f"Took {round(time.time() - s)}s") # Takes about 121s
+with pd.option_context("display.max_columns", None):
+    print(df)
+df.to_csv("overture_NZ.csv", index=False)
 
 df["lng"] = df.geometry.apply(lambda p: p.x)
 df["lat"] = df.geometry.apply(lambda p: p.y)
 
-df["name"] = df.names.apply(lambda d: d["common"][0]["value"])
-df["main_category"] = df.categories.apply(lambda c: c.main)
-df["alternate_categories"] = df.categories.apply(lambda c: c.alternate)
+df["name"] = df.names.apply(lambda c: c.primary)
+df["main_category"] = df.categories.apply(lambda c: c.main if c else None)
+df["alternate_categories"] = df.categories.apply(lambda c: c.alternate if c else None)
 
-df[["name", "main_category", "alternate_categories", "lat", "lng"]].to_csv("overture_CH.csv", index=False)
+df[["name", "main_category", "alternate_categories", "lat", "lng"]].to_csv("overture_NZ.csv", index=False)
